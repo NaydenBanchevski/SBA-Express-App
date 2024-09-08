86cfd2ce0ba4cb452c3706abdc8322d6a3c4dd0c
Nayden Banchevski <163778420+NaydenBanchevski@users.noreply.github.com>
2024-09-08 18:41:04 -0500

    Add view engine, install ejs, simple ejs form. Add public folder with styles.css file. Post Route for new user, and /:id route to find user by ID

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,22 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json(users);
 });
+
+router.post("/", (req, res) => {
+  //   console.log(req.body);
+  const newUser = { id: users.length + 1, ...req.body };
+  //   console.log(newUser);
+  users.push(newUser);
+  res.json(users);
+});
+
+router.get("/:id", (req, res) => {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  //   res.send("Hello World");
+  res.render("index");
 });
 
 app.use("/users", userRoutes);
